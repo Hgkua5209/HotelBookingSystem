0d7248356040443dfb0f8ae5ae21ec73f570cd79
Taki <126129738+Hgkua5209@users.noreply.github.com>
2026-06-11 22:23:52 +0800

    Fixing gitignore and cleaning untracked build files

--- HotelBookingSystem-ejb/build/generated-sources/ap-source-output/entities/Rooms_.java
+++ HotelBookingSystem-ejb/build/generated-sources/ap-source-output/entities/Rooms_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2026-06-11T22:10:23")
+@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2026-06-11T22:21:42")
 @StaticMetamodel(Rooms.class)
 public class Rooms_ { 
 
