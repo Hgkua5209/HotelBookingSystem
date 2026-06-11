04a7b45154c35f9213981d86fb80a6147bc5c9db
Taki <126129738+Hgkua5209@users.noreply.github.com>
2026-06-11 22:25:01 +0800

    Add built artifacts and update generated dates
    Removed build/ and HotelBookingSystem-war/dist/ from .gitignore and commit generated/compiled artifacts (EJB/WAR JARs, WAR, EAR and build/META-INF/MANIFEST.MF). Also updated EclipseLink @Generated timestamps in JPA metamodel classes (Bookings_, Cart_, Rooms_, Users_). This change makes build outputs and updated generated-sources tracked in the repository.

--- HotelBookingSystem-ejb/build/generated-sources/ap-source-output/entities/Cart_.java
+++ HotelBookingSystem-ejb/build/generated-sources/ap-source-output/entities/Cart_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2026-06-11T22:21:42")
+@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2026-06-11T22:24:24")
 @StaticMetamodel(Cart.class)
 public class Cart_ { 
 
--- HotelBookingSystem-ejb/build/generated-sources/ap-source-output/entities/Users_.java
+++ HotelBookingSystem-ejb/build/generated-sources/ap-source-output/entities/Users_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2026-06-11T22:21:42")
+@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2026-06-11T22:24:24")
 @StaticMetamodel(Users.class)
 public class Users_ { 
 
