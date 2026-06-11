47ae4578535802bf4cb123d6c9d607ada5c101e3
Taki <126129738+Hgkua5209@users.noreply.github.com>
2026-06-11 22:27:38 +0800

    System: Successfully activated gitignore and removed build artifacts

--- HotelBookingSystem-ejb/build/generated-sources/ap-source-output/entities/Bookings_.java
+++ HotelBookingSystem-ejb/build/generated-sources/ap-source-output/entities/Bookings_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2026-06-11T22:24:24")
+@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2026-06-11T22:25:31")
 @StaticMetamodel(Bookings.class)
 public class Bookings_ { 
 
