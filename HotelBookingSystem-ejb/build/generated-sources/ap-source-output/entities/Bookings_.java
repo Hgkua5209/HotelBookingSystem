65dda525bf844953c5a3fa499ecc32f2de0c41b7
Taki <126129738+Hgkua5209@users.noreply.github.com>
2026-06-11 21:09:32 +0800

    Update .gitignore and remove build/dist artifacts
    Improve .gitignore with clearer NetBeans, build, generated-sources and packaging ignores and add specific project paths. Remove numerous generated/compiled and deployment artifacts (build classes, generated metamodel timestamps updated, jars/wars, gfdeploy deployment metadata, manifests and temporary files) to keep the repository clean of build products and prevent committing transient files.

--- HotelBookingSystem-ejb/build/generated-sources/ap-source-output/entities/Bookings_.java
+++ HotelBookingSystem-ejb/build/generated-sources/ap-source-output/entities/Bookings_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2026-06-02T18:30:38")
+@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2026-06-11T21:04:38")
 @StaticMetamodel(Bookings.class)
 public class Bookings_ { 
 
